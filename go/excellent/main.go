cb5c829422be0225aa9abe7db32d8d2cd0d6d036
rooreckless <rooreckless@gmail.com>
2026-06-14 02:14:54 +0900

    go言語のテストのワークフロー(PR作成で起動)をためす

---
+++ go/excellent/main.go
@@ -0,0 +1,15 @@
+package main
+
+func EvenOrOdd(number int) string {
+	if number%2 == 0 {
+		return "Even"
+	} else {
+		return "Odd"
+	}
+}
+
+func main() {
+	// 例: 4は偶数、5は奇数
+	println(EvenOrOdd(4)) // 出力: Even
+	println(EvenOrOdd(5)) // 出力: Odd
+}
